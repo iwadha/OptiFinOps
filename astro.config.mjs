ba4330cbf34f90e8369ed253703e92f78a24ab87
iwadha <109955423+iwadha@users.noreply.github.com>
2024-12-25 00:00:24 +0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,13 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
 
 export default defineConfig({
   integrations: [tailwind(), react()],
-  site: "https://iwadha.github.io",
-  base: "OptiFinOps/",
+  site: 'https://iwadha.github.io',
+  base: '/OptiFinOps',  // Remove trailing slash
+  output: 'static',
   build: {
-    output: "static",
-    crawler: false,
-  },
-  vite: {
-    build: {
-      rollupOptions: {
-        output: {
-          entryFileNames: "[name].js",
-          chunkFileNames: "[name].js",
-          assetFileNames: "[name][extname]",
-        },
-      },
-    },
-  },
+    assets: '_assets'
+  }
 });
